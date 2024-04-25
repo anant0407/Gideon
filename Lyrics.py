aed7098ac4d030af795efaa67b318dd8b74e2b90
anant <anant0407@gmail.com>
2024-04-25 17:59:13 -0400

    requires google api key

--- Lyrics.py
+++ Lyrics.py
@@ -1,8 +1,9 @@
-# AIzaSyBn6SBnBd1Ru36L1lKLSW7WD9gCEt8vqRk
+# Google API Key will have to be put in the comment below and then uncommented to enable this feature to work appropriately
 
 from lyrics_extractor import SongLyrics
 
-extract_lyrics = SongLyrics("AIzaSyBn6SBnBd1Ru36L1lKLSW7WD9gCEt8vqRk","ac7f664d971914e3f")
+# extract_lyrics = SongLyrics("Google API Key here" "ac7f664d971914e3f")
+
 
 song = extract_lyrics.get_lyrics("")
 
